b3941ae581b536404cf51f8fd46a55128c2ac3c8
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2017-12-21 00:44:44 +0100

    Menu drawer modified : * removed : the tools item * renamed : take a picture, import from gallery, feedbacks

--- app/src/main/java/com/example/memorism/MainMenu.java
+++ app/src/main/java/com/example/memorism/MainMenu.java
@@ -86,8 +86,6 @@ public class MainMenu extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
