9d333e57af73383ce641baf50985f275502d9cd2
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2017-12-21 14:03:47 +0100

    activity name fixed

--- app/src/main/java/com/example/memorism/MainMenu.java
+++ app/src/main/java/com/example/memorism/MainMenu.java
@@ -1,5 +1,6 @@
 package com.example.memorism;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -82,6 +83,10 @@ public class MainMenu extends AppCompatActivity
 
         if (id == R.id.nav_create_journey) {
             // Handle the camera action
+            Intent intent = new Intent(this,create_journey.class);
+            startActivity(intent);
+
+
         } else if (id == R.id.nav_edit_journey) {
 
         } else if (id == R.id.nav_list_places) {
