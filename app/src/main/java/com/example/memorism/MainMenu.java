0791974bb7bd7ad97dc68484104b6642f9ffa02a
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2017-12-21 02:12:04 +0100

    Create journey item added in app drawer

--- app/src/main/java/com/example/memorism/MainMenu.java
+++ app/src/main/java/com/example/memorism/MainMenu.java
@@ -80,7 +80,7 @@ public class MainMenu extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.create_journey) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
