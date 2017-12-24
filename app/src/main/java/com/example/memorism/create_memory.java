21a18fe73231cdb30a873f87b0df6da9abe34204
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2017-12-24 02:32:37 +0100

    some add and modification

--- app/src/main/java/com/example/memorism/create_memory.java
+++ app/src/main/java/com/example/memorism/create_memory.java
@@ -5,7 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.memorism.dummy.DummyContent;
 
 public class create_memory extends AppCompatActivity {
 
@@ -21,8 +25,23 @@ public class create_memory extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Snackbar.make(view, "Memory saved !", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                EditText text = (EditText)findViewById(R.id.detail_input);
+                String detail_string_value = text.getText().toString();
+
+                text = (EditText)findViewById(R.id.memory_input);
+                String memory_string_value = text.getText().toString();
+                String new_id = String.valueOf(DummyContent.ITEMS.size());
+
+                DummyContent.DummyItem tmp_dummy = new DummyContent.DummyItem(new_id,memory_string_value,detail_string_value);
+
+                DummyContent.addItem(tmp_dummy);
+
+                /*Snackbar.make(view, "Memory saved !", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+
+
+
             }
         });
     }
