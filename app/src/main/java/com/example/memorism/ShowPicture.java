5b2d01ac27267a8ff099fbc1c55141b35c6df80f
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2017-12-27 05:01:29 +0100

    Picture is rotated well

--- app/src/main/java/com/example/memorism/ShowPicture.java
+++ app/src/main/java/com/example/memorism/ShowPicture.java
@@ -2,6 +2,7 @@ package com.example.memorism;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
@@ -26,17 +27,27 @@ public class ShowPicture extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         try{
+
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
             File root= new File(Environment.getExternalStorageDirectory()+File.separator+"MyMemorism");
 
             ImageView imageview = (ImageView) findViewById(R.id.picture);
 
             Bitmap bMap = BitmapFactory.decodeFile(root+File.separator+ ItemDetailFragment.mItem.date+".jpg");
-            imageview.setImageBitmap(bMap);
+            imageview.setImageBitmap(RotateBitmap(bMap,90));
         } catch (Exception e)
         {
             Log.e("SD Card","Error while trying to load picture",e);
         }
 
+    }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 
 }
