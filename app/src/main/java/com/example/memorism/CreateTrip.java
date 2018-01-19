8811e319157cc471590f968b30c12208039939e3
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-01-20 00:46:51 +0100

    Activity create_trip is created

---
+++ app/src/main/java/com/example/memorism/CreateTrip.java
@@ -0,0 +1,13 @@
+package com.example.memorism;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_trip);
+    }
+}
