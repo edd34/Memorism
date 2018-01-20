79de906cd3a381b388e2a8ddd21a05bf2f87af56
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-01-20 15:15:13 +0100

    In activity create trip : trip_name_name_prompted is assigned to what the user has written on the relevant field

--- app/src/main/java/com/example/memorism/create_trip.java
+++ app/src/main/java/com/example/memorism/create_trip.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.memorism.memory.MemoryContent;
 
 public class create_trip extends AppCompatActivity {
 
@@ -14,10 +17,15 @@ public class create_trip extends AppCompatActivity {
         setContentView(R.layout.activity_create_trip);
         final Intent intent_create_memory = new Intent(this,create_memory.class);
         Button button_createNewTrip = (Button) findViewById(R.id.button_done_trip_name_create);
+
+        final EditText text = (EditText)findViewById(R.id.trip_name_input_string);
+
+
         button_createNewTrip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
+                MemoryContent.trip_name_prompted= text.getText().toString();
                 startActivity(intent_create_memory);
             }
         });
