b34ab22fda17cc851683f4c8b59bc0e26532da3f
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-01-20 15:47:44 +0100

    fixing some bug now have to implement somethings correctly

--- app/src/main/java/com/example/memorism/TripListActivity.java
+++ app/src/main/java/com/example/memorism/TripListActivity.java
@@ -1,11 +1,25 @@
 package com.example.memorism;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.memorism.memory.MemoryContent;
+
+import com.example.memorism.memory.MemoryContent;
+
+import java.util.List;
+
 
 public class TripListActivity extends AppCompatActivity {
 
@@ -24,5 +38,83 @@ public class TripListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        View recyclerView = findViewById(R.id.item_list);
+        assert recyclerView != null;
+
     }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new TripListActivity.SimpleItemRecyclerViewAdapter(this, MemoryContent.ITEMS, false));
+    }
+
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<TripListActivity.SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final TripListActivity mParentActivity;
+        private final List<MemoryContent.MemoryItem> mValues;
+        private final boolean mTwoPane;
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MemoryContent.MemoryItem item = (MemoryContent.MemoryItem) view.getTag();
+                if (mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.date);
+                    ItemDetailFragment fragment = new ItemDetailFragment();
+                    fragment.setArguments(arguments);
+                    mParentActivity.getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.item_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, ItemDetailActivity.class);
+                    intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, item.date);
+
+                    context.startActivity(intent);
+                }
+            }
+        };
+
+        SimpleItemRecyclerViewAdapter(TripListActivity parent,
+                                      List<MemoryContent.MemoryItem> items,
+                                      boolean twoPane) {
+            mValues = items;
+            mParentActivity = parent;
+            mTwoPane = twoPane;
+        }
+
+        @Override
+        public TripListActivity.SimpleItemRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.trip_list_content, parent, false);
+            return new TripListActivity.SimpleItemRecyclerViewAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final TripListActivity.SimpleItemRecyclerViewAdapter.ViewHolder holder, int position) {
+            holder.mIdView.setText(mValues.get(position).date);
+            holder.mContentView.setText(mValues.get(position).title);
+
+            holder.itemView.setTag(mValues.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final TextView mIdView;
+            final TextView mContentView;
+
+            ViewHolder(View view) {
+                super(view);
+                mIdView = (TextView) view.findViewById(R.id.id_text);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+        }
+    }
+
 }
